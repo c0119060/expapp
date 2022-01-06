547ec267fc69caa99e4dec9d6df7463a896beaa6
TouiAndou <74704982+TouiAndou@users.noreply.github.com>
2022-01-06 12:11:48 +0900

    Toui Commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
 	  res.end('hello world');
+	  res.end('こんにちは')
 });
 
 server.listen(PORT, () => {
